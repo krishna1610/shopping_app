be85d03c576f0a41c8ba4774df81b183167ce009
Krishna <krishna@Krishnas-MacBook-Pro.local>
2021-02-09 11:23:51 -0800

    input and create button added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,47 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  render(){
-    return(
-      <main></main>
+  constructor(props) {
+    super(props);
+    this.state = {
+      queryText: ''
+    };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleTextChange(event) {
+    this.setState({ queryText: event.target.value });
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    this.setState({
+      queryText: ''
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <header>
+          <div className="container my-5">
+            <form onSubmit={this.handleFormSubmit}>
+              <div className="d-flex">
+                <div className="p-2 flex-grow-1">
+                  <input type="text" className="form-control" placeholder="Type here..."
+                    value={this.state.queryText} onChange={this.handleTextChange} />
+                </div>
+                <div className="p-2">
+                  <input type="submit" className="btn btn-primary" value="Create" disabled={!this.state.queryText} />
+                </div>
+              </div>
+            </form>
+          </div>
+        </header>
+      </main>
     );
   }
 }
